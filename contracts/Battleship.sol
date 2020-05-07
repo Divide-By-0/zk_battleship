e26254e4552208d1be4ad046043a48ef5dfd81d1
Jason Zhao <jason@Jasons-MacBook-Pro.local>
2020-05-06 23:39:54 -0700

    slight progress

--- contracts/Battleship.sol
+++ contracts/Battleship.sol
@@ -4,9 +4,12 @@ contract Battleship {
 
     uint64 constant n_shipsquares = 17;
     uint64 constant INVALID_SHOT = 999;
+    uint64 constant g = 69;
+    uint64 constant p = 999983;
 
-    uint n_games = 0;
-    uint n_activegames = 0;
+    uint64 n_games = 0;
+    uint64 n_activegames = 0;
+    uint64 last_id = 1;
     
     struct Game {
         address p1;
@@ -19,20 +22,27 @@ contract Battleship {
         uint64 turn; // 1 for p1, 2 for p2
         bool active;
         bool exists; // bool defaults to false
+        uint64 winner; // 0 for noone, 1 for p1 wins, 2 for p2 wins
     }
+
+    // TODO: write function that kills inactive/finished games after a time limit
 
     // return values not allowed within functions that modify state
     // so we need to use events to notify game creator of the game id
     event GameIDs(address indexed from, uint gameID);
 
-    mapping(uint => Game) private games;
+    mapping(uint => Game) games;
 
-    function declare() external returns(uint) {
+    function declare() external {
         // Create new game for a user and give them gameID 
-        // TODO: randomize gameID as 5 digit number or something
 
-        // TODO: gameID randomize
-        uint gameID = 123456;
+        // pseudorandom gameID
+        uint64 gameID = (last_id * g) % p;
+        while (games[gameID].exists) {
+            gameID = (gameID + 1) % p;
+        }
+        last_id = gameID;
+
         Game storage game = games[gameID];
 
         game.p1 = msg.sender;
@@ -45,10 +55,9 @@ contract Battleship {
         game.exists = true;
         game.candidateShot = INVALID_SHOT;
 
-        emit GameIDs(msg.sender, gameID);
-
         n_games++;
-        return gameID;
+
+        emit GameIDs(msg.sender, gameID);
     }
 
     function join(uint gameID) external {
@@ -97,8 +106,8 @@ contract Battleship {
     function verify(uint64 proof, uint result) internal pure returns(bool) {
         return true;
     }
-    // verify proof and end current turn
-    function resolveShot(uint gameID, uint64 proof, uint64 result) external returns(bool) {
+    // verify proof and end current turn. potentially end game
+    function resolveShot(uint gameID, uint64 proof, uint64 result) external {
         Game storage game = games[gameID];
         // check ships placed, it's opponent's turn, opponent already shot, result is valid
         require(game.active && game.board1 != 0 && game.board2 != 0);
@@ -117,13 +126,38 @@ contract Battleship {
             game.turn = 3 - game.turn;
             game.candidateShot = INVALID_SHOT;
         }
-        // returns true if successfully verifies proof and update was made
-        return verified;
+        checkGameEnd(gameID);
     }
 
-    //TODO: check game end condition based on # of hits
-    //TODO: end game and reset states if finished
+    // check game end condition based on # of hits
+    // end the game if the game has finished
+    function checkGameEnd(uint gameID) internal returns(uint64){
+        Game storage game = games[gameID];
+        uint64 p1_hits = 0;
+        uint64 p2_hits = 0;
+        for (uint64 i = 0; i < 100; i++) {
+            if (game.revealed1[i] == 2) {
+                p1_hits++;
+            }
+            if (game.revealed2[i] == 2) {
+                p2_hits++;
+            }
+        }
+        if (p1_hits == n_shipsquares) {
+            game.winner = 1;
+        }
+        if (p2_hits == n_shipsquares) {
+            game.winner = 2;
+        }
+    }
 
-    // NOTE: we don't need getter functions because private variables only prevent 
-    // other contracts from accessing info, not the whole world
+    function killGame(uint gameID) external {
+        Game memory game = games[gameID];
+        require(msg.sender == game.p1 || msg.sender == game.p2);
+        delete games[gameID];
+    }
+
+    // On second thought it might be easier to implement a getter function
+    // for convenience even though the game state is public
+    // (we can return just one game not everything)
 }
--- test/BattleshipTest.js
+++ test/BattleshipTest.js
@@ -1,10 +1,20 @@
 const Battleship = artifacts.require("./Battleship.sol");
 
 contract("Battleship", accounts => {
-	it("Joined valid game", async () => {
+	it("Joined multiple valid games", async () => {
 		const battleship = await Battleship.deployed();
-		let result = await battleship.declare({from: accounts[0]}); // user 0 creates game
-		let gameID = result.logs[0].args["gameID"].toNumber(); // get gameID from event log
-		await battleship.join(gameID, {from: accounts[1]}); // user 1 joins game
+		for(i = 0; i < 3; i++) {
+			let result = await battleship.declare({from: accounts[0]}); 
+			let gameID = result.logs[0].args["gameID"].toNumber(); 
+			await battleship.join(gameID, {from: accounts[1]}); 
+		}
+	});
+	it("Placing ships", async () => {
+		const battleship = await Battleship.deployed();
+		let result = await battleship.declare({from: accounts[0]}); 
+		let gameID = result.logs[0].args["gameID"].toNumber(); 
+		await battleship.join(gameID, {from: accounts[1]}); 
+		await battleship.place(gameID, 1, {from: accounts[0]});
+		await battleship.place(gameID, 2, {from: accounts[1]});
 	});
 });
